b8171d6374477d6087b39d51628ccc2cd96012dd
Ostap Blystsiv <ostapcr7@gmail.com>
2022-01-09 21:13:43 +0200

    add

---
+++ index.js
@@ -0,0 +1,43 @@
+// 1. https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// function createPhoneNumber(numbers) {
+	
+// }
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+// 2.  https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// function likes(names) {
+
+// }
+// console.log(likes(["Peter"]));
+
+// 3. https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript
+// Examples: 
+// spinWords("Hey fellow warriors") => returns "Hey wollef sroirraw"
+// spinWords("This is a test") => returns "This is a test"
+// spinWords("This is another test") => returns "This is rehtona test"
+// function spinWords(string) {
+
+// }
+// console.log(spinWords("This is another test"));
+
+// 4. https://www.codewars.com/kata/517abf86da9663f1d2000003/train/javascript
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+// function toCamelCase(str){
+
+// }
+// console.log(toCamelCase("The-Stealth-Warrior"));
+
+// 5. https://www.codewars.com/kata/5324945e2ece5e1f32000370/train/javascript
+// sumStrings('1','2') // => '3'
+// function sumStrings(a,b) { 
+
+// }
+// console.log(sumStrings('1','2'));
