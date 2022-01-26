05e1f7381847024eca45010cefc3f3ade927fa95
Ostap Blystsiv <ostapcr7@gmail.com>
2022-01-26 23:03:09 +0200

    add

--- index.js
+++ index.js
@@ -105,7 +105,7 @@
 
 // !==========================================================================
 
-// Moving Zeros To The End(5 kyu)
+// Moving Zeros To The End(5 kyu)✅
 // 6. https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 // var moveZeros = function (arr) {
